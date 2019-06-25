ad1638bd20193811c4df0691840aaadad6ea5438
406803045 <406803045@qq.com>
2019-06-25 11:19:12 +0800

    优化，首次启动打包，安装gulp-cli解决npm i安装报错

--- config.js
+++ config.js
@@ -10,15 +10,15 @@
  * @data    2019-03-05
  */
 module.exports = {
-  enabledQiniu: false, // 是否开启七牛云COS 上传功能
-  // 七牛 上传功能配置表
-  qiniu: {
-    accessKey: 'xxx',
-    secretKey: 'xxx',
-    bucket: 'xxx',
-    prefix: 'weapp',
-    private: false
-  },
-  // 静态资源CDN 域名，配合CDN 功能实用，线上请确保在mp管理端已经注册域名
-  assetsCDN: 'https://tweapp.top1buyer.com/'
+	enabledQiniu: true, // 是否开启七牛云COS 上传功能
+	// 七牛 上传功能配置表
+	qiniu: {
+		accessKey: 'xxx',
+		secretKey: 'xxx',
+		bucket: 'xxx',
+		prefix: 'weapp',
+		private: false
+	},
+	// 静态资源CDN 域名，配合CDN 功能实用，线上请确保在mp管理端已经注册域名
+	assetsCDN: 'https://tweapp.top1buyer.com/'
 }
